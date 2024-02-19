3c2bd63439f2a271851ddcb2504b9f172295c0f0
Fatima Badaoui <fatimina615@gmail.com>
2024-02-19 16:09:32 +0100

    add page not found

--- src/routes/PageNotFound/index.jsx
+++ src/routes/PageNotFound/index.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>ERROR: Page Not Found!</h1>
+    <div className="page-not-found">
+      <div className="error-message">
+        <h1>OPPS...</h1>
+        <h2>Sorry. this page is not found.</h2>
+        <img
+          src="https://bonx-vue.pages.dev/images/others/404.webp"
+          alt="error 404"
+        />
+        <Link to="/">GO BACK HOME</Link>
+      </div>
     </div>
   );
 };
