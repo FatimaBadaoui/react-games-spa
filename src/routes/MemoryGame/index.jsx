9f43a743c3508fd84591ddb564c626992ac2279d
Fatima Badaoui <fatimina615@gmail.com>
2024-02-23 11:05:31 +0100

    make games responsive

--- src/routes/MemoryGame/index.jsx
+++ src/routes/MemoryGame/index.jsx
@@ -1,24 +1,11 @@
 import "./style.css";
 import Cards from "./components/Cards.jsx";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
 
 function MemoryGame() {
-
-  const navigate = useNavigate();
-
-  const clickHandler = () => {
-    navigate("/games");
-  };
-
   return (
     <>
       <h1 className="memory-title">Memory Game</h1>
       <div className="main-container">
-        <button className="reload-btn" onClick={clickHandler}>
-        <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
         <Cards />
       </div>
     </>
